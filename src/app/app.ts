1aa5df3ce1e940c1cff12da12cacabf8bc345457
Sunil Poudel <suneelpdl90@gmail.com>
2025-07-26 09:03:08 +0545

    link menu template to app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Header} from './header/header';
+import {Menu} from './menu/menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Menu],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
