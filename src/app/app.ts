f9b7df0ae478875aebdc35a229ac5cb78b651f9c
Sunil Poudel <suneelpdl90@gmail.com>
2025-07-26 09:07:24 +0545

    link body component with app

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Header} from './header/header';
 import {Menu} from './menu/menu';
+import {Body} from './body/body';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Menu],
+  imports: [RouterOutlet, Header, Menu, Body],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
