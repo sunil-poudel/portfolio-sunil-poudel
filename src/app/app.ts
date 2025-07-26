541205dcab0caf869fd143db07a4e05159e23485
Sunil Poudel <suneelpdl90@gmail.com>
2025-07-26 09:10:42 +0545

    add content to footer and link to app

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import {Header} from './header/header';
 import {Menu} from './menu/menu';
 import {Body} from './body/body';
+import {Footer} from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Menu, Body],
+  imports: [RouterOutlet, Header, Menu, Body, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
