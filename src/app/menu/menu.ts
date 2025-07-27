85b35245ac34c1f6756af3ff4d3bc3a69f9efacf
Sunil Poudel <suneelpdl90@gmail.com>
2025-07-27 18:24:13 +0545

    inject portfolio services to menu

--- src/app/menu/menu.ts
+++ src/app/menu/menu.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {PortfolioServices} from '../shared/portfolio.services';
 
 @Component({
   selector: 'app-menu',
@@ -7,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './menu.css'
 })
 export class Menu {
+  portfolioServices = inject(PortfolioServices);
 
 }
