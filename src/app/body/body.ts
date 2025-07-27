29c968aa9d8648f19335b23dbae5e0a11ccbfda9
Sunil Poudel <suneelpdl90@gmail.com>
2025-07-27 18:33:53 +0545

    link about and skills to be displayed in body

--- src/app/body/body.ts
+++ src/app/body/body.ts
@@ -1,9 +1,14 @@
 import {Component, inject} from '@angular/core';
 import {PortfolioServices} from '../shared/portfolio.services';
+import {About} from '../menu/about/about';
+import {Skills} from '../menu/skills/skills';
 
 @Component({
   selector: 'app-body',
-  imports: [],
+  imports: [
+    About,
+    Skills
+  ],
   templateUrl: './body.html',
   styleUrl: './body.css'
 })
