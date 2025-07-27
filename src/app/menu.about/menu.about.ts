f33b7ab3a2a8155893bffabc128a3798c5894c5d
Sunil Poudel <suneelpdl90@gmail.com>
2025-07-27 18:19:04 +0545

    generate menu.about and menu.skills components

---
+++ src/app/menu.about/menu.about.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu.about',
+  imports: [],
+  templateUrl: './menu.about.html',
+  styleUrl: './menu.about.css'
+})
+export class MenuAbout {
+
+}
---
+++ src/app/menu.skills/menu.skills.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu.skills',
+  imports: [],
+  templateUrl: './menu.skills.html',
+  styleUrl: './menu.skills.css'
+})
+export class MenuSkills {
+
+}
