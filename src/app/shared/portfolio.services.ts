1d43ebc8ea021ffafbd054c14bc1c90598b41ee1
Sunil Poudel <suneelpdl90@gmail.com>
2025-07-27 18:23:06 +0545

    create writable signal to get current click status

--- src/app/shared/portfolio.services.ts
+++ src/app/shared/portfolio.services.ts
@@ -1,6 +1,8 @@
-import {Injectable} from '@angular/core';
+import {Injectable, signal, WritableSignal} from '@angular/core';
 
 @Injectable({providedIn: 'root'})
 export class PortfolioServices{
+  currentClickStatus: WritableSignal<'about'|'skills'|'home'> = signal('about');
+
 
 }
