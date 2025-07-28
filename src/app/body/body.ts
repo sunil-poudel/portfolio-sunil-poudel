ffe08ad87992b043b82654e2ce9be24d41538de3
Sunil Poudel <suneelpdl90@gmail.com>
2025-07-28 14:55:00 +0545

    link projects menu to body

--- src/app/body/body.ts
+++ src/app/body/body.ts
@@ -2,12 +2,14 @@ import {Component, inject} from '@angular/core';
 import {PortfolioServices} from '../shared/portfolio.services';
 import {About} from '../menu/about/about';
 import {Skills} from '../menu/skills/skills';
+import {Projects} from '../menu/projects/projects';
 
 @Component({
   selector: 'app-body',
   imports: [
     About,
-    Skills
+    Skills,
+    Projects
   ],
   templateUrl: './body.html',
   styleUrl: './body.css'
